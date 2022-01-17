e3b5edfce13f59d3fe21b7a4e57d5594e41ca42b
Anzhelika Chernykh <anzhelika.chernykh@edu.fh-joanneum.at>
2022-01-17 19:31:59 +0100

    rework sound.py

--- test/Sound/sound.py
+++ test/Sound/sound.py
@@ -11,8 +11,8 @@ class Sound:
             Configures the mixer to play and load needed sounds
         """
         mixer.init()
-        self.shoot_sound = mixer.Sound('test_1.mp3')
-        self.score_sound = mixer.Sound('test_2.mp3')
+        self.shoot_sound = mixer.Sound('/home/pi/sounds_effect/laser-shoot.wav')
+        self.score_sound = mixer.Sound('intro.wav')
        
     def play_sound(self, event: int):
         """
@@ -21,11 +21,13 @@ class Sound:
         if event == 1:
             self.shoot_sound.set_volume(10)
             self.shoot_sound.play()
+
         elif event == 2:
             self.score_sound.set_volume(10)
             self.score_sound.play() 
 
-    def stop_sound(self, event: int)
+
+    def stop_sound(self, event: int):
         """
             Stops sound depending on the event
         """
@@ -38,5 +40,9 @@ if __name__ == "__main__":
     test = Sound()
     
     test.play_sound(1)
-    time.sleep(5) #just for the test
+    time.sleep(3) #just for the test
     test.stop_sound(1)
+    time.sleep(2)
+    test.play_sound(2)
+    time.sleep(10) #just for the test
+    test.stop_sound(2)
